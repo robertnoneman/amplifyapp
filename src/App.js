5c53e1b908ddfb094c774001177af6d6fb158ec1
robertnoneman <robertnoneman@gmail.com>
2020-09-13 17:33:38 -0400

    Error somewhere, this might not fix it

--- src/App.js
+++ src/App.js
@@ -31,8 +31,32 @@ function App() {
   async function deleteNote({ id }) {
     const newNotesArray = notes.filter(note => note.id !== id);
     setNotes(newNotesArray);
-    await API.graphql({ query: deleteNoteMutation, variables: { input: { id }}});
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
   }
+  /* const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listNotes });
+    setNotes(apiData.data.listNotes.items);
+  }
+
+  async function createNote() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+    setNotes([ ...notes, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteNote({ id }) {
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id }}});
+  } */
 
   return (
     <div className="App">
