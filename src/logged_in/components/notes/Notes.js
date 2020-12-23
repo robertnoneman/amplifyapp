895a3d54da9a4504a5e4f6be1dcba955550b4102
robertnoneman <robertnoneman@gmail.com>
2020-12-23 13:37:20 -0500

    Layout and styling on notes page

--- src/logged_in/components/notes/Notes.js
+++ src/logged_in/components/notes/Notes.js
@@ -7,6 +7,7 @@ import PropTypes from "prop-types";
 import { 
   Box,
   Button,
+  Card,
   Divider,
   Grid,
   Paper,
@@ -16,11 +17,20 @@ import {
   withStyles,
 } from "@material-ui/core"
 import classNames from "classnames";
+import { color } from "@material-ui/system";
 
 
 const styles = (theme) => ({
   card: {
     boxShadow: theme.shadows[2],
+    paddingTop: theme.spacing(6),
+    paddingBottom: theme.spacing(6),
+    paddingLeft: theme.spacing(4),
+    paddingRight: theme.spacing(4),
+    border: `1px solid ${theme.palette.secondary.main}`,
+    borderRadius: theme.shape.borderRadius * 2,
+    background: 'linear-gradient(30deg, #2196f325 30%, #21cbf325 90%)',
+    //marginTop: theme.spacing(2),
   },
   title: {
     transition: theme.transitions.create(["background-color"], {
@@ -36,6 +46,11 @@ const styles = (theme) => ({
       color: theme.palette.primary.dark,
     },
   },
+  divider: {
+    color: theme.palette.primary.main,
+    backgroundColor: theme.palette.primary.main,
+    margin: theme.spacing(1),
+  }
 });
 
 const initialFormState = { name: '', description: ''};
@@ -67,72 +82,72 @@ function Notes(props) {
     fetchNotes();
   }, []);
 
-  // useEffect(() => {
-  //   selectNotes();
-  // }, [selectNotes]);
-
   useEffect(selectNotes, [selectNotes]);
 
   return (
     <Fragment>
-      <Grid spacing={0} container>
-        <Grid
-          item
-          xs={12}
-          sm={12}
-          lg={12}
-          className="relative"
-        >
-          <TextField
-            fullWidth
-            multiline
-            variant="outlined"
-            rows={1}
-            className="text-white"
-            onChange={e => setFormData({...formData, 'name': e.target.value})}
-            placeholder="Note name"
-            value={formData.name}
-          />
+      <Paper>
+        <Grid spacing={0} container>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            lg={12}
+            className="relative"
+          >
+            <TextField
+              fullWidth
+              multiline
+              variant="outlined"
+              rows={1}
+              className="text-white"
+              onChange={e => setFormData({...formData, 'name': e.target.value})}
+              placeholder="Note name"
+              value={formData.name}
+            />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            lg={12}
+            className="relative"
+          >
+            <TextField
+              fullWidth
+              multiline
+              variant="outlined"
+              rows={3}
+              className="text-white"
+              onChange={e => setFormData({...formData, 'description': e.target.value})}
+              placeholder="Note description"
+              value={formData.description}
+            />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            lg={12}
+            className="relative"
+          >
+            <Button 
+              variant="contained"
+              color="secondary"
+              className="text-white"
+              onClick={createNote}>Create Note
+            </Button>
+          </Grid>
         </Grid>
-        <Grid
-          item
-          xs={12}
-          sm={12}
-          lg={12}
-          className="relative"
-        >
-          <TextField
-            fullWidth
-            multiline
-            variant="outlined"
-            rows={3}
-            className="text-white"
-            onChange={e => setFormData({...formData, 'description': e.target.value})}
-            placeholder="Note description"
-            value={formData.description}
-          />
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          sm={12}
-          lg={12}
-          className="relative"
-        >
-          <Button 
-            variant="contained"
-            color="secondary"
-            className="text-white"
-            onClick={createNote}>Create Note
-          </Button>
-        </Grid>
-
-        <Paper>
+      </Paper>
+      <Paper className="lg-mg-top">
+        <Grid container spacing={1}>
           <Toolbar>
-            <Typography variant="h6">Your Notes</Typography>
+            <Typography variant="h4">Your Notes</Typography>
             <Button
               variant="contained"
               color="secondary"
+              style={{display: "none"}}
             >
               Non-functional button
             </Button>
@@ -143,22 +158,25 @@ function Notes(props) {
               {
                 notes.map(note => (
                   <Grid item xs={6} sm={4} md={3} key={note.id || note.name}>
-                    <Typography variant="h3">{note.name}</Typography>
-                    <Typography>{note.description}</Typography>
-                    <Button 
-                      variant="contained"
-                      color="secondary"
-                      onClick={() => deleteNote(note)}
-                    >
-                      Delete note
-                    </Button>
+                    <Card className={classes.card}>
+                      <Typography variant="h5">{note.name}</Typography>
+                      <Divider className={classes.divider} style={{color: theme.palette.primary.main}, {backgroundColor: theme.palette.primary.main}}/>
+                      <Typography align="left" gutterBottom>{note.description}</Typography>
+                      <Button 
+                        variant="contained"
+                        color="secondary"
+                        onClick={() => deleteNote(note)}
+                      >
+                        Delete note
+                      </Button>
+                    </Card>
                   </Grid>
                 ))
               }
             </Grid>
           </Box>
-        </Paper>
-      </Grid>
+        </Grid>
+      </Paper>
     </Fragment>
   )
 };
